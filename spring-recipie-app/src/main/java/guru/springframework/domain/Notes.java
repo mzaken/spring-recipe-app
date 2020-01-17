d4c12ff14146e41add9c21d9caa09e268a477143
Maor Zaken <maorzaken@gmail.com>
2020-01-17 08:47:20 +0200

    Created Recipe and Notes Entities.

--- spring-recipie-app/src/main/java/guru/springframework/domain/Notes.java
+++ spring-recipie-app/src/main/java/guru/springframework/domain/Notes.java
@@ -19,4 +19,20 @@ public class Notes {
 	
 	@OneToOne
 	private Recipe recipe;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
 }
--- spring-recipie-app/src/main/java/guru/springframework/domain/Recipe.java
+++ spring-recipie-app/src/main/java/guru/springframework/domain/Recipe.java
@@ -8,6 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
+/**
+ * @author Maor Zaken
+ * Created on Jan 17, 2020
+ */
 @Entity
 public class Recipe {
 	
@@ -23,10 +27,94 @@ public class Recipe {
 	private String url;
 	private String directions;
 	
-	//TOTO Difficulty
+	//TODO: Difficulty
+	//private Difficulty difficulty;
+	
 	@Lob
 	private Byte[] image;
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(Integer prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public Integer getCookTime() {
+		return cookTime;
+	}
+
+	public void setCookTime(Integer cookTime) {
+		this.cookTime = cookTime;
+	}
+
+	public Integer getServings() {
+		return servings;
+	}
+
+	public void setServings(Integer servings) {
+		this.servings = servings;
+	}
+
+	public String getSource() {
+		return Source;
+	}
+
+	public void setSource(String source) {
+		Source = source;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+
+	public Byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+
+	public Notes getNotes() {
+		return notes;
+	}
+
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+	}
 }
+
+
