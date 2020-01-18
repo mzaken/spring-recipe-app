213b3996c892b10aada127f19190d9d9933174bf
maor.zaken <maorzaken@gmail.com>
2020-01-18 15:15:13 +0200

    Added category entity.

--- spring-recipie-app/src/main/java/guru/springframework/domain/Recipe.java
+++ spring-recipie-app/src/main/java/guru/springframework/domain/Recipe.java
@@ -9,7 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -43,6 +46,12 @@ public class Recipe {
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category",
+			joinColumns = @JoinColumn(name = "recipe_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 
 	public Long getId() {
 		return id;
@@ -140,5 +149,3 @@ public class Recipe {
 		this.difficulty = difficulty;
 	}
 }
-
-
