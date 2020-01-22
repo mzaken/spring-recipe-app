c760c46674173aa54f4bf51cdd5c7d319439af4b
maorza <maorza@Maor-win10.cmr-bll.co.il>
2020-01-22 14:05:11 +0200

    Added UnitOfMeasureRepository integration tests.

---
+++ spring-recipie-app/src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package guru.springframework.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import guru.springframework.domain.UnitOfMeasure;
+
+/**
+ * @author Maor Zaken
+ * Created on 22 Jan 2020
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+	
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void findByDescription() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+	void findByDescriptionCup() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+		
+		assertEquals("Cup", uomOptional.get().getDescription());
+	}
+
+}
