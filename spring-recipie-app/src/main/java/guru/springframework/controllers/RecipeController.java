98ee16582dbfc90968dfe2644b26fe49bbf9dea0
maor.zaken <maorzaken@gmail.com>
2020-01-28 07:24:16 +0200

    Added Recipe form page and update RecipeController for create.

--- spring-recipie-app/src/main/java/guru/springframework/controllers/RecipeController.java
+++ spring-recipie-app/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -5,9 +5,13 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 
 /**
@@ -28,5 +32,20 @@ public class RecipeController {
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
 		
 		return "recipe/show";
+	}
+	
+	@RequestMapping("/recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 }
