7a9176bb309dfda3dbdadea4bfc67314ae6f6dc9
maorza <maorza@Maor-win10.cmr-bll.co.il>
2020-02-05 14:04:22 +0200

    Fixed testSaveOrUpdate test in IngredientControllerTest.

--- spring-recipie-app/src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ spring-recipie-app/src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -109,7 +109,7 @@ public class IngredientControllerTest {
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.param("id", "")
                 .param("description", "some string"))
-				.andExpect(view().name("redirect:recipe/2/ingredient/3/show"))
+				.andExpect(view().name("redirect:/recipe/2/ingredient/3/show"))
 				.andExpect(status().is3xxRedirection());
 			
 				
