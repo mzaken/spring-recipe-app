d1067a7cfb20c483245aaf2391cdecd319b72846
maorza <maorza@Maor-win10.cmr-bll.co.il>
2020-02-06 11:32:49 +0200

    aa

--- spring-recipie-app/src/main/java/guru/springframework/domain/Ingredient.java
+++ spring-recipie-app/src/main/java/guru/springframework/domain/Ingredient.java
@@ -13,13 +13,15 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Maor Zaken Created on Jan 17, 2020
  */
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
